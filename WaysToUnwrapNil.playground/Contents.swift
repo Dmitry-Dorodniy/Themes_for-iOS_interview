ed7bb4131e98007f8a9ce2bad238cc3cd3daa708
DmitryDorodniy <dmitrydorodniy@gmail.com>
2022-04-21 13:30:57 +0300

    Four ways added

--- WaysToUnwrapNil.playground/Contents.swift
+++ WaysToUnwrapNil.playground/Contents.swift
@@ -1,7 +1,36 @@
 import Foundation
 
-var text: String? = "some text"
+//var text: String?
 //nil
+var text: String? = "some text"
+//some text
 
 //1. Принудительно (forced) - не безопасно
+//var unwrapped1: String = text!
 
+//2. Неявно (implicitly) - не безопасно
+//var unwrapped2 = text!
+
+//3. Опциональное привязывание (binding) - безопасно
+if let unwrapped3 = text {
+    print("optional succesfully unwrapped: \(unwrapped3)")
+} else {
+    print("nil here")
+}
+
+//in Swift 5.7
+//If let text {
+//    print("optional succesfully unwrapped: \(unwrapped3)")
+//} else {
+//    print("nil here")
+//}
+
+
+//4. Опциональная цепочка (chaining) - безопасно
+let unwrapped4 = text?.uppercased()
+print(unwrapped4 as Any)
+//Optional("SOME TEXT")
+//nil -- if nil inn text
+
+
+//5.
