14220bf42ce2b08e6289134c0d95bb8fba86776c
DmitryDorodniy <dmitrydorodniy@gmail.com>
2022-04-21 16:05:38 +0300

    Finish

--- WaysToUnwrapNil.playground/Contents.swift
+++ WaysToUnwrapNil.playground/Contents.swift
@@ -9,7 +9,7 @@ var text: String? = "some text"
 //var unwrapped1: String = text!
 
 //2. Неявно (implicitly) - не безопасно
-//var unwrapped2 = text!
+//var unwrapped2: String! = text
 
 //3. Опциональное привязывание (binding) - безопасно
 if let unwrapped3 = text {
@@ -30,7 +30,27 @@ if let unwrapped3 = text {
 let unwrapped4 = text?.uppercased()
 print(unwrapped4 as Any)
 //Optional("SOME TEXT")
-//nil -- if nil inn text
+//nil -- if nil in text
 
 
-//5.
+//5. Значение по умолчанию (nil coalescing) - безопасно
+let unwrapped5 = text ?? "no text"
+print(unwrapped5)
+//some text
+//no text -- if nil in text
+
+//6. С помощью guart let в функциях
+func someFunc() {
+    guard let text = text else { print("nil here"); return }
+    print("optional succesfully unwrapped: \(text)")
+}
+
+someFunc()
+
+
+//7. Optional pattern
+if case let unwrapped7? = text {
+    print(unwrapped7)
+} else {
+    print("nil here")
+}
